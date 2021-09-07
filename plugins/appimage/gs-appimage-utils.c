a55603fb52290d26b8c692ef48c0dd731b1a6463
pktiuk <kotiuk@zohomail.eu>
2021-09-07 15:12:18 +0200

    Add separate file with plugin utilities

---
+++ plugins/appimage/gs-appimage-utils.c
@@ -0,0 +1,6 @@
+#include "gs-appimage-utils.h"
+
+gboolean load_from_desktop_file (GsApp *app, gchar *desktop_filepath)
+{
+	return TRUE;
+}
---
+++ plugins/appimage/gs-appimage-utils.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <gnome-software.h>
+
+gboolean load_from_desktop_file (GsApp *app, gchar *desktop_filepath);
